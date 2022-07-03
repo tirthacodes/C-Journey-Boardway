5fadeb53fd92d8d9341bf92cbe80e13c859cf1f5
Tirth <tirthacodes@gmail.com>
2022-07-04 00:23:59 +0545

    Product.cs class created

--- ConsoleApp.DbConnections/FirstWinFormsApp/Form1.Designer.cs
+++ ConsoleApp.DbConnections/FirstWinFormsApp/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Infos";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
